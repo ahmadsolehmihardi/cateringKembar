658a390f90c7fcaf2b6c9d2aabbf1b1c1de9c697
ahmadsolehmihardi <ahmadsolehmihardi@gmail.com>
2025-06-20 00:22:22 +0700

    Update vite.config.ts
    base : di rubah dengan hanya /

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-   base: "/cateringKembar/",
+   base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
